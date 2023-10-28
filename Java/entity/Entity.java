fce9f876c3ee66d44f5bc02b50b36d04305e63d0
BenoitKremer <benoit.kremer@epitech.eu>
2023-10-28 23:19:38 +0200

    1st Sprite moving

--- Java/entity/Entity.java
+++ Java/entity/Entity.java
@@ -1,8 +1,16 @@
 package Java.entity;
 
+import java.awt.image.BufferedImage;
+
 public class Entity {
 
     public int x, y;
     public int speed;
     
+    //Buffered permet de gérer et manipuler les données d'un image
+    public BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    public String direction;
+
+    public int spriteCounter = 0;
+    public int spriteNum = 1;
 }
--- Java/entity/Player.java
+++ Java/entity/Player.java
@@ -2,11 +2,16 @@ package Java.entity;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 import Java.main.GamePanel;
 import Java.main.KeyHandler;
 
 
-
+// liaison avec Entity, GamePanel et KeyHandler
 public class Player extends Entity{
 
     GamePanel gp;
@@ -17,34 +22,125 @@ public class Player extends Entity{
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
-    
+
+    //Données du Player
     public void setDefaultValues() {
 
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
     }
+
+    //Images du Player
+    public void getPlayerImage() {
+
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightUpAFK.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightUpMove.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightDownAFK.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightDownMove.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightLeftAFK.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightLeftMove.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightRightAFK.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/res/Player/KnightRightMove.png"));
+
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    //Permet les mouvements du Player
     public void update() {
-        //Permet les mouvements du joueur
+
+        //cela permet d'animer le sprite seulement lorsque qu'il se déplace
+        if(keyH.upPressed == true
+         || keyH.downPressed == true 
+         || keyH.leftPressed == true 
+         || keyH.rightPressed == true){
+
+        /*Cette partie permet "d'animer" le sprite 
+         * c'est ce qui va faire le changement entre
+        la frame(image) 1 et la frame 2 */
+        spriteCounter++;
+        if(spriteCounter > 10){ //Le 10 permet de changer toutes les 10frames DU JEU donc à 60 FPS 6 changements par secondes
+            if(spriteNum ==1){
+                spriteNum = 2;
+            }
+            else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+            
+         }
+        
         if(keyH.upPressed == true) {
+            direction = "up";
             y -= speed;
         }
         else if (keyH.downPressed == true){
+            direction = "down";
             y += speed;
         }
         else if (keyH.leftPressed == true){
+            direction = "left";
             x -= speed;
         }
         else if (keyH.rightPressed == true){
+            direction = "right";
             x += speed;
         }
+
+        
     }
+
+    //Partie graphique (ou draw)
     public void draw(Graphics2D g2) {
-        // Met une couleur dans le graph
-        g2.setColor(Color.white);
-        // Permet de remplir une zone à l'aide de coordonnées
-        // Au format (coord.X, coord.Y, Width, Height)
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        //Contrôle de l'affichage en fonction de la direction
+        BufferedImage image = null;
+
+        switch(direction){
+        case "up":
+            if(spriteNum == 1){
+                image = up1; //Frame1
+            }
+            if(spriteNum == 2){
+                image = up2; //Frame2
+            }
+            break;
+        case "down":
+            if(spriteNum == 1){
+                image = down1;
+            }
+            if(spriteNum == 2){
+                image = down2;
+            }
+            break;
+        case "left":
+            if(spriteNum == 1){
+                image = left1;
+            }
+            if(spriteNum == 2){
+                image = left2;
+            }
+            break;
+        case "right":
+            if(spriteNum == 1){
+                image = right1;
+            }
+            if(spriteNum == 2){
+                image = right2;
+            }
+            break;
+        
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
