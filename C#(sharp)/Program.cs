2f7e31ad9aea52d5440574e2a4ed8a76e442e51f
BenoitKremer <benoit.kremer@epitech.eu>
2023-11-03 16:03:32 +0100

    JAVA

--- C#(sharp)/Program.cs
+++ C#(sharp)/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+    // Références:
+    // in = lecture seule
+    // ref = lecture + écriture
+    // out = déclaration de l'extérieur
+
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number = 5;
+        int number2 = 10;
+
+        Console.WriteLine(number);
+        Console.WriteLine(number2);
+
+        ResetNumbers(number, ref number2);
+
+        Console.WriteLine(number);
+        Console.WriteLine(number2);
+    }
+    public static void ResetNumbers(in int nb, ref int nb2)
+    {
+        Console.WriteLine(nb);
+        nb2 = 0;
+    }
+}
